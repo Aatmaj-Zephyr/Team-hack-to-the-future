704b8a4ab1903f4afed5b3b3726c844cd36e3e00
Ritvik25 <86009242+Ritvik25@users.noreply.github.com>
2022-04-09 18:29:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,27 +18,48 @@ def press(self):
 #global data
 #create a float layout
 
-def Camera_on_Layout(self):
-    hide_widget(Turn_on_CCTV,True)
-    hide_widget(Turn_off_CCTV,False)
 
-def bell_press():
+def decline(self):
+    Camera_on_Layout(self)
+def accept(self):
+    Camera_on_Layout(self)
+def unknown_bell_press():
     #add sounds here
     print("bell pressed")
     hide_widget(Turn_on_CCTV,True)
     hide_widget(Turn_off_CCTV,False)
-    hide_widget(warning_Button,True)
-    hide_widget(warning_label,True)
-    hide_widget(accept_Button,True)
-    hide_widget(accept_label,True)
-    hide_widget(decline_Button,True)
-    hide_widget(decline_label,True)
-    hide_widget(person_label,True)
+    hide_widget(warning_Button,False)
+    hide_widget(warning_label,False)
+    hide_widget(accept_Button,False)
+    hide_widget(accept_label,False)
+    hide_widget(decline_Button,False)
+    hide_widget(decline_label,False)
+    hide_widget(person_label,False)
     hide_widget(background_image_box_accept,True)
-    hide_widget(background_image_box_decline,True)
-    hide_widget(Bell,True)
-    hide_widget(background_image,True)
+    hide_widget(background_image_box_decline,False)
+    hide_widget(Bell,False)
+    hide_widget(background_image,False)
     hide_widget(person_float_layout,False)
+def set_person(person_label):
+    person_label.text = "Raju Bhaji"
+def known_bell_press():
+    #add sounds here
+    print("bell pressed")
+    hide_widget(Turn_off_CCTV, False)
+    hide_widget(warning_Button, True)
+    hide_widget(warning_label, True)
+    hide_widget(accept_Button, False)
+    hide_widget(accept_label, False)
+    hide_widget(decline_Button, False)
+    hide_widget(decline_label, False)
+    hide_widget(person_label, False)
+    hide_widget(background_image_box_accept, False)
+    hide_widget(background_image_box_decline, True)
+
+    hide_widget(Bell, False)
+    hide_widget(background_image, False)
+    hide_widget(person_float_layout, False)
+    set_person(person_label)
 
 #create class for the main window
 def Camera_off_Layout(self):
@@ -54,8 +75,10 @@ def Camera_off_Layout(self):
     hide_widget(background_image_box_accept,True)
     hide_widget(background_image_box_decline,True)
     hide_widget(Bell,True)
-    print("h")
-
+def Camera_on_Layout(self):
+    hide_widget(Turn_on_CCTV,True)
+    hide_widget(Turn_off_CCTV,False)
+    unknown_bell_press()
 def blank():
     pass
 def hide_widget(wid, dohide=True):
@@ -81,24 +104,24 @@ person_float_layout.add_widget(background_image_box_accept)
 background_image_box_decline = Image(source='unknown.jpeg', allow_stretch=True, keep_ratio=False,
                              opacity=0.3, size_hint=(.8, .6), pos_hint={'center_x': 0.5, 'center_y': 0.5})
 person_float_layout.add_widget(background_image_box_decline)
-person_label = Label(text="Unidentified Access!!",font_size=55,color="blue", size_hint=(.2, .2), pos_hint={'center_x':0.5, 'center_y':0.5})
+person_label = Label(text="Unidentified Access!!",bold=True, font_size=60,color="red", size_hint=(.2, .2), pos_hint={'center_x':0.5, 'center_y':0.5})
 person_float_layout.add_widget(person_label)
 accept_Button=Image(source ='icons8-login-64.png',pos_hint={'center_x':0.8, 'center_y':0.30})
 person_float_layout.add_widget(accept_Button)
-accept_label=Button(text="accept",background_color=(0, 0, 0, 0),size_hint=(0.20, 0.15),on_press=press,font_size=35,bold=True,color="green",pos_hint={'center_x':0.8, 'center_y':0.25})
+accept_label=Button(text="accept",background_color=(0, 0, 0, 0),size_hint=(0.20, 0.15),on_press=accept,font_size=40,bold=True,color="green",pos_hint={'center_x':0.8, 'center_y':0.25})
 person_float_layout.add_widget(accept_label)
 
 
 decline_Button = Image(source='icons8-close-64.png', on_press=press,size_hint=(.2, .2),
                       pos_hint={'center_x': 0.2, 'center_y': 0.30})
 person_float_layout.add_widget(decline_Button)
-decline_label = Button(text="decline",background_color=(0, 0, 0, 0),size_hint=(0.20, 0.15),on_press=press,font_size=35,bold=True, color="red",  pos_hint={'center_x': 0.2, 'center_y': 0.25})
+decline_label = Button(text="decline",background_color=(0, 0, 0, 0),size_hint=(0.20, 0.15),on_press=decline,font_size=40,bold=True, color="red",  pos_hint={'center_x': 0.2, 'center_y': 0.25})
 person_float_layout.add_widget(decline_label)
 
 warning_Button = Image(source='icons8-warning-64.png',
                        pos_hint={'center_x': 0.5, 'center_y': 0.70})
 person_float_layout.add_widget(warning_Button)
-warning_label = Label(text="Warning!!", bold=True,font_size=35,color="red", size_hint=(.1, .1), pos_hint={'center_x': 0.5, 'center_y': 0.65})
+warning_label = Label(text="Warning!!", bold=True,font_size=40,color="red", size_hint=(.1, .1), pos_hint={'center_x': 0.5, 'center_y': 0.65})
 person_float_layout.add_widget(warning_label)
 
 Turn_on_CCTV = Button(text="Turn on CCTV", on_press=Camera_on_Layout,size_hint=(.5, .1),pos_hint={'center_x': 0.5, 'center_y': 0.2},background_color=(0,1,0,1))
