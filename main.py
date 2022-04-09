b6d87adbf49e891756f1a0fa4dbc90f29ee2a17b
Rahul Dandona <87532892+dandonarahul2002@users.noreply.github.com>
2022-04-09 15:49:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ def initiate_Normal_layout(float_layout):
     person_float_layout = FloatLayout()
     background_image = AsyncImage(source='wallpaper.jpeg', allow_stretch=True, keep_ratio=False)
     person_float_layout.add_widget(background_image)
-    background_image = Image(source ='Unknown wallpaper.jpeg',on_touch_down=press,allow_stretch=True, keep_ratio=False,opacity = 0.3,size_hint=(.8, .6),pos_hint={'center_x':0.5, 'center_y':0.5})
+    background_image = Image(source ='Unknown wallpaper.jpeg',on_touch_down=press,allow_stretch=True, keep_ratio=False,opacity = 0.3,size_hint=(.8, .5),pos_hint={'center_x':0.5, 'center_y':0.6})
     person_float_layout.add_widget(background_image)
-    person_label = Label(text="Unidentified Acess!!", font_size=50,color="blue", size_hint=(.2, .2), pos_hint={'center_x':0.5, 'center_y':0.5})
+    person_label = Label(text="Unidentified Access!!",font_size=55,color="blue", size_hint=(.2, .2), pos_hint={'center_x':0.5, 'center_y':0.5})
     person_float_layout.add_widget(person_label)
    # accept_Button= Button(text="accept",size_hint=(.1,.1),pos_hint={'center_x':0.8, 'center_y':0.30},background_color=(0,1,0,1))
     accept_Button=Image(source ='icons8-login-64.png',on_touch_down=press,pos_hint={'center_x':0.8, 'center_y':0.30})
     person_float_layout.add_widget(accept_Button)
-    accept_label=Label(text="accept",bold=True,color="green",size_hint=(.1,.1),pos_hint={'center_x':0.8, 'center_y':0.25})
+    accept_label=Label(text="accept",font_size=35,bold=True,color="green",size_hint=(.1,.1),pos_hint={'center_x':0.8, 'center_y':0.25})
     person_float_layout.add_widget(accept_label)
 
 
     decline_Button = Image(source='icons8-close-64.png', on_touch_down=press,
                           pos_hint={'center_x': 0.2, 'center_y': 0.30})
     person_float_layout.add_widget(decline_Button)
-    decline_label = Label(text="decline",bold=True, color="red", size_hint=(.1, .1), pos_hint={'center_x': 0.2, 'center_y': 0.25})
+    decline_label = Label(text="decline",font_size=35,bold=True, color="red", size_hint=(.1, .1), pos_hint={'center_x': 0.2, 'center_y': 0.25})
     person_float_layout.add_widget(decline_label)
 
     decline_Button = Image(source='icons8-warning-64.png', on_touch_down=press,
                            pos_hint={'center_x': 0.5, 'center_y': 0.70})
     person_float_layout.add_widget(decline_Button)
-    decline_label = Label(text="Warning!!", bold=True,color="red", size_hint=(.1, .1), pos_hint={'center_x': 0.5, 'center_y': 0.65})
+    decline_label = Label(text="Warning!!", bold=True,font_size=35,color="red", size_hint=(.1, .1), pos_hint={'center_x': 0.5, 'center_y': 0.65})
     person_float_layout.add_widget(decline_label)
 
     Turn_on_CCTV = Button(text="Turn on CCTV", size_hint=(.5, .2),pos_hint={'center_x': 0.5, 'center_y': .5},background_color=(0,1,0,1))
